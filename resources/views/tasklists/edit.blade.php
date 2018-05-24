81de0fc4472ee47df06e31547441545b39547900
kazuki.usui <kazuki.a.usui@rakuten.com>
2018-05-24 05:28:13 +0000

    CRUD tasklists

---
+++ resources/views/tasklists/edit.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<h1>id: {{ $tasklist->id }} の編集ページ</h1>
+
+    {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put']) !!}
+
+        {!! Form::label('content', 'メッセージ:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('更新') !!}
+
+    {!! Form::close() !!}
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasklistsController@index');
+
+
+Route::resource('tasklists', 'TasklistsController');
+
+
+Route::get('tasklists/{id}/copy', 'TasklistsController@copy');
+
+
