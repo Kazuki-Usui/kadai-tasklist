b9f9ce08ab700de20812fcbd798265f184a11a58
kazuki.usui <kazuki.a.usui@rakuten.com>
2018-05-30 06:16:25 +0000

    bootstrap design

--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -5,8 +5,7 @@
 
 <h1>id = {{ $tasklist->id }} の詳細ページ</h1>
 
-<div class="row">
-  <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+  <div class=" row col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
     <p>タイトル{{$tasklist->title}}</p>
     <p>ステータス{{ $tasklist->status }}</p>
@@ -22,7 +21,6 @@
       {!! Form::close() !!}
     
 
-</div>
 </div>
     
 @endsection
